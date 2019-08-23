de30abca3ae33d8678427da819e752cbad687e0d
Kenny Arehart <kennyarehart@gmail.com>
2019-08-23 00:09:06 -0700

    converted Hummingbird.js to a hybrid instance/static; removed SingleBird as unnecessary; moved engine methods to manager.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 export { default as Hummingbird } from './lib/Hummingbird'
+// export { default as Manager } from 'hummingbird/lib/proto/Manager.js'
+// import Item from './lib/proto/Item'
+// export { Item }
