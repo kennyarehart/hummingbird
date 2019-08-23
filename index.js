bacf3126557760ddeed1375332ad92fb64a09670
Kenny Arehart <kennyarehart@gmail.com>
2019-08-23 13:27:32 -0700

    renamed Category to Cadence; added public methods to single bird instance

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
 export { default as Hummingbird } from './lib/Hummingbird'
-// export { default as Manager } from 'hummingbird/lib/proto/Manager.js'
-// import Item from './lib/proto/Item'
-// export { Item }
