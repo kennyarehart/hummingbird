d4a1adbf12c358fcf78578113f17300d351a3761
Kenny Arehart <kennyarehart@gmail.com>
2019-09-05 23:51:47 -0700

    added support for onComplete/onCompleteParams for lifeSpan

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 export { default as Hummingbird } from './lib/Hummingbird'
 export { default as Flock } from './lib/Flock'
-export { SECONDS, FLAPS } from './lib/constants'
+export { FLAPS, SECONDS } from './lib/constants'
